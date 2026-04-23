d28c063af04f1d98aa1f3afa5c1eb1a85ae877e0
anuanakalil2006-cloud <anuanakalil2006@gmail.com>
2026-04-23 18:10:19 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,24 +1,47 @@
-export default function Dashboard() {
-  return (
-    <div className="flex">
-      
-      {/* Sidebar */}
-      <div className="w-64 bg-black text-white h-screen p-4">
-        <h2 className="text-xl mb-4">Admin Panel</h2>
-        <ul>
-          <li className="mb-2 cursor-pointer">Dashboard</li>
-          <li className="mb-2 cursor-pointer">Resellers</li>
-          <li className="mb-2 cursor-pointer">Orders</li>
-          <li className="mb-2 cursor-pointer">Settings</li>
-        </ul>
-      </div>
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-      {/* Content */}
-      <div className="p-6 flex-1">
-        <h1 className="text-2xl font-bold">Welcome Admin 👋</h1>
-        <p className="mt-2 text-gray-600">
-          Manage your resellers, orders, and reports here.
-        </p>
+export default function Login() {
+  const router = useRouter();
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleLogin = () => {
+    if (user === "admin" && pass === "1234") {
+      router.push("/dashboard");
+    } else {
+      alert("Invalid Login");
+    }
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-xl shadow-md w-80">
+        <h2 className="text-xl font-bold mb-4 text-center">
+          Reseller Admin Login
+        </h2>
+
+        <input
+          type="text"
+          placeholder="Username"
+          className="w-full border p-2 mb-2"
+          onChange={(e) => setUser(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border p-2 mb-4"
+          onChange={(e) => setPass(e.target.value)}
+        />
+
+        <button
+          onClick={handleLogin}
+          className="w-full bg-blue-500 text-white p-2 rounded"
+        >
+          Login
+        </button>
       </div>
     </div>
   );
