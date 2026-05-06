58ff5504f2415d924c320c6ed4028ce1e8297f75
anuanakalil2006-cloud <anuanakalil2006@gmail.com>
2026-05-06 12:34:42 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,48 +1,5 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { redirect } from "next/navigation";
 
-export default function Login() {
-  const router = useRouter();
-  const [user, setUser] = useState("");
-  const [pass, setPass] = useState("");
-
-  const handleLogin = () => {
-    if (user === "admin" && pass === "1234") {
-      router.push("/dashboard");
-    } else {
-      alert("Invalid Login");
-    }
-  };
-
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded-xl shadow-md w-80">
-        <h2 className="text-xl font-bold mb-4 text-center">
-          Reseller Admin Login
-        </h2>
-
-        <input
-          type="text"
-          placeholder="Username"
-          className="w-full border p-2 mb-2"
-          onChange={(e) => setUser(e.target.value)}
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border p-2 mb-4"
-          onChange={(e) => setPass(e.target.value)}
-        />
-
-        <button
-          onClick={handleLogin}
-          className="w-full bg-blue-500 text-white p-2 rounded"
-        >
-          Login
-        </button>
-      </div>
-    </div>
-  );
+export default function Home() {
+  redirect("/login");
 }
